067b8b7e7afcae61235d8d3209884f3a1aa5293d
Sebastian Luna <eliaslunaslo@gmail.com>
2026-02-19 21:42:19 -0300

    FEAT: implementacion de logica update y ruta de actualizacion. Fixed #7

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -5,5 +5,6 @@ const projectController = require('../controllers/projectController');
 router.get('/', projectController.getAll);
 router.post('/', projectController.create);
 router.delete('/:id', projectController.delete);
+router.put('/:id', projectController.edit);
 
 module.exports = router;
